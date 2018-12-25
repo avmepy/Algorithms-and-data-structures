7e6aac6dc9b044e6e21377f21b453b366f96a6a9
Valentyn <valentyn.kofanov@knu.ua>
2018-12-25 22:02:34 +0200

    refactoring

--- LinearStructure/Queue.py
+++ LinearStructure/Queue.py
@@ -5,22 +5,72 @@
 
 
 class NodeForQueue:
-    def __init__(self, item):
-        self.key = item
-        self.next = None
+    """
+    auxiliary class
+    implement node of queue
+    """
+    def __init__(self, item=None):
+        self.item = item  # load (value) element
+        self.next = None  # link to next item in queue
 
 
 class Queue:
+    """
+    class Queue
+    """
     def __init__(self):
-        self.mFront = None
-        self.mBack = None
-        self.size = 0
+        """
+        constructor
+        """
+        self.m_front = None  # the first item
+        self.m_back = None  # the last item
+        self.size = 0  # length of queue
 
     def empty(self):
-        return self.size == 0
+        """
+        :return: True - if queue is empty / False - otherwise
+        """
+        return self.m_front is None and self.m_back is None
 
     def push(self, item):
-        pass
+        """
+        add item to queue
+        :param item: item
+        :return:
+        """
+        self.size += 1
+        new_node = NodeForQueue(item)
+        if self.empty():
+            self.m_front = new_node
+        else:
+            self.m_back.next = new_node
+        self.m_back = new_node
+
+    def pop(self):
+        """
+        extract item from queue
+        :return: the first item
+        """
+        assert not self.empty(), 'Queue is empty!'
+        current_front = self.m_front
+        item = current_front.item
+        self.m_front = self.m_front.next
+        del current_front
+        self.size -= 1
+        if self.m_front is None:
+            self.m_back = None
+        return item
+
+    def front(self):
+        """
+        return the first item without extraction
+        :return: the first item
+        """
+        assert not self.empty(), 'Queue is empty!'
+        return self.m_front.item
 
     def __len__(self):
+        """
+        :return: size of queue
+        """
         return self.size
