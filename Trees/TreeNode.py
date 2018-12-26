72d9a79c6d279821d1e505ef084ba198a761e70a
Valentyn <valentyn.kofanov@knu.ua>
2018-12-26 15:36:40 +0200

    update TreeNode.py

--- Trees/TreeNode.py
+++ Trees/TreeNode.py
@@ -7,19 +7,22 @@
 class TreeNode:
     """
     auxiliary class
-    implements the node of tree
+    implements the node of the tree
     """
-    def __init__(self, key=None):
+    def __init__(self, key=None, father=None):
         """
         constructor
         :param key: the key of the node
+        :param father: the father of the node (link)
         """
         self.key = key
+        self.father = father
+        self.children = []
 
     def empty(self):
         """
         check for emptiness
-        :return:
+        :return: True / False
         """
         return self.key is None
 
@@ -27,9 +30,17 @@ class TreeNode:
         """
         sets the node key
         :param newKey: new key
-        :return:
+        :return: None
         """
         self.key = newKey
+
+    def addChild(self, child):
+        """
+        adds child
+        :param child: child (type: TreeNode)
+        :return: None
+        """
+        self.children.append(child)
 
     def __str__(self):
         """
@@ -44,3 +55,9 @@ class TreeNode:
         :return: the node key
         """
         return str(self.key)
+
+    def hasChildren(self):
+        """
+        :return: True if node has children / False otherwise
+        """
+        return self.children != []
